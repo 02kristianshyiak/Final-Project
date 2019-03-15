1ae207db979fb484b958979265d19b9e530a63ce
02kristianshyiak <48028427+02kristianshyiak@users.noreply.github.com>
2019-03-15 07:48:35 -0400

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -2,22 +2,22 @@
 # Final Project
 # Kristian Shyiak
 # 3.7.19
-# This program is doing
+# In this program you type in your name and you get a number 
 
 
-name = input("What is your name: ")
+name = input("What is your name: ")  #This is where you type your name
 
-def greeting():
-    print("Hi there " + name + "!")
-    print("Nice to meet you, here are your numbers.")
-    for i in range(1, 50):
-        print("Number: " + str(i))
-        x = i
-        if i == 3:
+def greeting():      # This is where the function starts
+    print("Hi there " + name + "!") # This prints "Hi there" and the typed in name
+    print("Nice to meet you, here are your numbers.")  # This prints "Nice to meet you, here are your numbers."
+    for i in range(1, 50):       # This starts the for loop
+        print("Number: " + str(i)) # This prints the number and the string
+        x = i   # This is x=i
+        if i == 3: # Both these lines tell when to break
             break
-        while x >= 0:
-            print(name + str(x))
-            x = x - 1
-        print("")
+        while x >= 0:       # This starts the while loop
+            print(name + str(x))    # This prints the name and the string
+            x = x - 1    # This tells how many lines there will be
+        print("")     # This helps print
 
 greeting()        # This makes all the code appear
