f098712b8118fecef79161028a75e3517ff47216
Kristian Shyiak <02kristian.shyiak@gulllakecs.org>
2019-03-15 08:26:25 -0400

    Add Function Part

--- FinalProject.py
+++ FinalProject.py
@@ -1,11 +1,19 @@
-''''''
-# Final Project
-# Kristian Shyiak
-# 3.7.19
-# This program is doing
+'''
+Final Project
+Kristian Shyiak
+3.7.19
+This program is doing
+'''
 
+try:
+    name = input("What is your name: ")
+    print('Your name:', name)
 
-name = input("What is your name: ")
+except ValueError:
+    print('\n''That is not a name!')
+
+# Function part
+# 3.15.19
 
 def greeting():
     print("Hi there " + name + "!")
@@ -17,7 +25,7 @@ def greeting():
             break
         while x >= 0:
             print(name + str(x))
-            x = x - 1
+            x = x - 2
         print("")
 
 greeting()        # This makes all the code appear
